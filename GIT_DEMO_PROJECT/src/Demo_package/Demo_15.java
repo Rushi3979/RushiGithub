f70c84b234675cf9445eaf40619c409feb100d15
Rushi <Rushi@DESKTOP-VPG85OJ>
2022-06-15 11:22:12 +0530

    By Rushi_second_method

--- GIT_DEMO_PROJECT/src/Demo_package/Demo_15.java
+++ GIT_DEMO_PROJECT/src/Demo_package/Demo_15.java
@@ -2,12 +2,16 @@ package Demo_package;
 
 public class Demo_15 {
 
-	Public void m1()
+	public void m1()
 	{
-		System.out.pritnln("Hello From Git Website m1 method");
+		System.out.println("Hello From Git Website m1 method");
 		
 	}
-	
+	public void m2()
+	{
+		System.out.println("Hello From Eclipse m2 method");
+		
+	}
 	
 	public static void main(String[] args) {
 		
