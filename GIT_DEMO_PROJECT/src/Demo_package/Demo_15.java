42891d8ac36dbc1c6ababa3efa83e02149bab21e
Rushi3979 <107499159+Rushi3979@users.noreply.github.com>
2022-06-15 11:18:15 +0530

    Update Demo_15.java

--- GIT_DEMO_PROJECT/src/Demo_package/Demo_15.java
+++ GIT_DEMO_PROJECT/src/Demo_package/Demo_15.java
@@ -2,6 +2,13 @@ package Demo_package;
 
 public class Demo_15 {
 
+	Public void m1()
+	{
+		System.out.pritnln("Hello From Git Website m1 method");
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git");
